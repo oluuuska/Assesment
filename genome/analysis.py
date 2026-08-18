47f117f771fabd0b9083cb64ac42c32eb2a3a5d1
oluuuska <staniecaleksandra@gmail.com>
2026-08-18 18:49:02 +0200

    Update analysis.py

--- genome/analysis.py
+++ genome/analysis.py
@@ -8,7 +8,7 @@ from statsmodels.tsa.stattools import coint, adfuller
 
 # A cointegration test on a handful of points is meaningless. Require a
 # reasonable number of overlapping observations before trusting a result.
-MIN_OBS_FOR_COINT = 20
+MIN_OBS_FOR_COINT = 5
 
 ACTOR_COL = "_queried_actor"
 
